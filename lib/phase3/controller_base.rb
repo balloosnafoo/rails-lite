bf7d8c58e65057fe0213b7ea72b6e0e95ff7c6a8
Matthew Skinner <skinnerm@email.arizona.edu>
2015-07-21 14:21:30 -0700

    Add basic erb rendering

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -8,6 +8,12 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      controller_name = self.class.to_s.underscore
+      html = ERB.new(
+        File.read("views/#{controller_name}/#{template_name}.html.erb")
+        ).result(binding)
+
+      render_content(html, "text/html")
     end
   end
 end
